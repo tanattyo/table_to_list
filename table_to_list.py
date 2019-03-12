6abef5519e39e0422263be797074ec1a41422e69
hayate.tanaka <tanattyokana@gmail.com>
2019-03-12 23:23:12 +0900

    create makeDict

--- table_to_list.py
+++ table_to_list.py
@@ -22,19 +22,42 @@ def main():
     url = 'test.csv'
     df = LoadCsv(url)
     label = SetRequiredLabel()
-    print('データセットの内訳は:' + str(df.shape))
+    text = makeDict(df, label)
+    print(text)
+
+def makeDict(df, label):
+
     # 種名によってグルーピング
-    grouped = df.groupby(label[0]).groups
+    grouped = df.groupby(label['label_species']).groups
+    all_species_list = {}
 
     for species in grouped:
-        print(species)
+        # ある種の標本情報の初期化
+        text_info_all_specimens = ''
+
         for specimen in grouped[species]:
+            # ある標本の情報の初期化
+            text_info_all_label = ''
+
             if specimen != 0:
-                i = df.loc[specimen]
-                for label_category in label[1]:
-                    print(i[label_category])
+                # ある標本のカテゴリの初期化
+                text_info_all_label = ''
+
+                key_label = df.loc[specimen]
+
+                for label_key in label['label_info']:
+                    text_info_all_label += str(key_label[label_key])
+                text_info_all_label += text_info_all_label
             else:
                 print('標本番号ないです')
+
+            text_info_all_specimens += text_info_all_label
+
+        all_specimen_list = {species: text_info_all_specimens}
+
+        all_species_list.update(all_specimen_list)
+
+    return all_species_list
 
 if __name__ == "__main__":
     main()
