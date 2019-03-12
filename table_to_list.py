39eb1a62427befd054be0f55b0eda1c07e73a5f6
hayate.tanaka <tanattyokana@gmail.com>
2019-03-13 00:37:10 +0900

    create formatingList

--- table_to_list.py
+++ table_to_list.py
@@ -17,14 +17,6 @@ def SetRequiredLabel():
     }
     return label
 
-def main():
-    # urlを設定する
-    url = 'test.csv'
-    df = LoadCsv(url)
-    label = SetRequiredLabel()
-    text = makeDict(df, label)
-    print(text)
-
 def makeDict(df, label):
 
     # 種名によってグルーピング
@@ -35,6 +27,7 @@ def makeDict(df, label):
         # ある種の標本情報の初期化
         text_info_all_specimens = ''
 
+        j = 0
         for specimen in grouped[species]:
             # ある標本の情報の初期化
             text_info_all_label = ''
@@ -45,19 +38,44 @@ def makeDict(df, label):
 
                 key_label = df.loc[specimen]
 
+                i = 0
                 for label_key in label['label_info']:
                     text_info_all_label += str(key_label[label_key])
-                text_info_all_label += text_info_all_label
+
+                    # label数-1の分だけ","を入れる
+                    if i == len(label['label_info'])-1:
+                        break
+                    text_info_all_label += ', '
+                    i += 1
             else:
                 print('標本番号ないです')
 
             text_info_all_specimens += text_info_all_label
+            # 標本数-1の分だけ";"を入れる
+            if j == len(grouped[species]) - 1:
+                break
+            text_info_all_specimens += '; '
+            j += 1
 
-        all_specimen_list = {species: text_info_all_specimens}
+        text_info_all_specimens_formatted = formatingList(text_info_all_specimens)
+        all_specimen_list = {species: text_info_all_specimens_formatted}
 
         all_species_list.update(all_specimen_list)
 
     return all_species_list
+
+def formatingList(text):
+
+    formatted_text = text.replace('\u3000', '')
+    return formatted_text
+
+def main():
+    # urlを設定する
+    url = 'test.csv'
+    df = LoadCsv(url)
+    label = SetRequiredLabel()
+    all_species_list = makeDict(df, label)
+    print(all_species_list)
 
 if __name__ == "__main__":
     main()
